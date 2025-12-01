4b1414e7308d3015f1b0464043f5a2453119447a
jesse <61173820+jessebeard@users.noreply.github.com>
2025-12-01 15:24:36 -0800

    refactor: add steganography module public API

---
+++ src/core/steganography/index.js
@@ -0,0 +1,4 @@
+// Public API for steganography module
+export { Jsteg } from './jsteg.js';
+export { ChunkManager } from './chunk-manager.js';
+export { ErrorCorrection } from './error-correction.js';
